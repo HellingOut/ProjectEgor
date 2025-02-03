d73d50ef1a4dec417a4ad5feb92da26bb37faa90
HellingOut <hellingout19@gmail.com>
2025-02-03 12:49:59 +0400

    Player movement

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,8 +1,15 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Scripting.APIUpdating;
 
 public class player : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int speed = 10;
+    public new Rigidbody2D rigidbody;
+
+    private float direction = 0;
     
     void Start()
     {
@@ -11,6 +18,10 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        direction = Input.GetAxis("Horizontal");
+        if (rigidbody != null && direction != 0)
+        {
+            rigidbody.linearVelocityX = direction * speed;
+        }
     }
 }
